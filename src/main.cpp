172c156bd88bc2fc769b7530d466368145c73371
te9yie <te9yie@users.noreply.github.com>
2022-12-20 08:48:22 +0900

    init

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+int main(int argc, char* argv[]) {
+#if defined(_MSC_VER)
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+  return 0;
+}
---
+++ src/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+#if defined(_MSC_VER)
+#include <crtdbg.h>
+#endif
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+#include <vulkan/vulkan.h>
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_vulkan.h"
